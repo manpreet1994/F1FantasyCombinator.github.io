b41ea8e46a056d29691dedb02be172c808cc6cac
manpreet94 <manpreet.1994@gmail.com>
2025-09-22 02:40:03 +0000

    running from root

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      plugins: [react()],
-      base: '/f1_fantasy_2025/',
+        plugins: [react()],
+        base: '/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)

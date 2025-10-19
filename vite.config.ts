81f24305c2c6bb8eab76263f31d83070bd70fc88
manpreet94 <manpreet.1994@gmail.com>
2025-10-19 07:08:05 +0000

    deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '//F1FantasyCombinator.github.io//',
+  base: "/F1FantasyCombinator.github.io/",
   plugins: [react()],
 })

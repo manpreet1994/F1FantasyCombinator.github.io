30317038be9e0b249723a7d772d834375777c3de
manpreet94 <manpreet.1994@gmail.com>
2025-10-20 06:54:18 +0000

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/F1FantasyCombinator.github.io/",
+  build : {
+    outDir: "dist"
+  },
   plugins: [react()],
 })
